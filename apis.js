92f576c6cba67192e9615e568bc72f20817a662c
Your Name <you@example.com>
2022-01-29 16:46:08 -0300

    Bka

--- apis.js
+++ apis.js
@@ -267,7 +267,17 @@ router.get('/others/fazernick', async(req, res, next) => {
           nick7: `${e[7]}`,
           nick8: `${e[8]}`,
           nick9: `${e[9]}`,
-          nick10: `${e[10]}`
+          nick10: `${e[10]}`,
+          nick10: `${e[11]}`,
+          nick10: `${e[12]}`,
+          nick10: `${e[13]}`,
+          nick10: `${e[14]}`,
+          nick10: `${e[15]}`,
+          nick10: `${e[16]}`,
+          nick10: `${e[17]}`,
+          nick10: `${e[18]}`,
+          nick10: `${e[19]}`,
+          nick10: `${e[20]}`
         }
       }
     )
