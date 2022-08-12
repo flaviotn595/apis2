9fceb24c5f14da4c167066483ef5256f99e55d51
JG-Bots <105572107+JG-Bots@users.noreply.github.com>
2022-08-12 06:03:29 -0300

    Update acont.js

--- public/acont.js
+++ public/acont.js
@@ -1,4 +1,4 @@
-function logar(){
+8function logar(){
 
 var login = document.getElementById('login').value;
 var senha = document.getElementById('senha').value;
@@ -12,6 +12,10 @@ var senha = document.getElementById('senha').value;
     }
 
     if(login == "DModZ" && senha == "holapapu132"){
+                location.href = "home.html";
+    }
+
+    if(login == "KazeDevID" && senha == "Kaze123"){
                 location.href = "home.html";
     }
 }
